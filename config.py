e8a2b5f3827352671070bdc1b8516ebbd57b727a
redoxin42-bit <redoxin42@gmail.com>
2026-05-21 07:24:54 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Путь: config.py
+import os
+
+TOKEN = "8198575429:AAEzgX1vxCZfABrwhZEXOf48kRdYv4_s3yY"
+ADMIN_ID = 123456789  # ЗАМЕНИ ТУТ НА СВОЙ ТЕЛЕГРАМ ID
+
+# Создаем необходимые папки при старте
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+USER_INSTANCES_DIR = os.path.join(BASE_DIR, "user_instances")
+DB_DIR = os.path.join(BASE_DIR, "database")
+
+os.makedirs(USER_INSTANCES_DIR, exist_ok=True)
+os.makedirs(DB_DIR, exist_ok=True)
+
+DB_PATH = os.path.join(DB_DIR, "qcode.db")
